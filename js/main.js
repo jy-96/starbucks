2be91d5afc733a5d76f47c0900551a432120da03
jaeyoonw <jaeyoon.dev@gmail.com>
2021-10-04 17:36:00 +0900

    Make section and btn

--- js/main.js
+++ js/main.js
@@ -15,3 +15,25 @@ searchInputEl.addEventListener('blur', function () {
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
 });
+
+// 함수가 계속 실행되는 것을 0.3초의 텀을 주는 역할
+// lodash라는 javascript library를 통해 throttle이라는 메소드 사용 가능
+// _.throttle(함수, 시간)
+const badgeEl = document.querySelector('header .badges');
+window.addEventListener('scroll', _.throttle(function () {
+    console.log(window.scrollY);
+    if(window.scrollY > 500) {
+        //배지 숨기기
+        // gasp.to(요소, 지속시간, 옵션); 
+        gsap.to(badgeEl, .6, {
+            opacity: 0,
+            display: 'none'
+        });
+    } else {
+        //배지 보이기
+        gsap.to(badgeEl, .6, {
+            opacity: 1,
+            display: 'block'
+        });
+    }
+}, 300));
